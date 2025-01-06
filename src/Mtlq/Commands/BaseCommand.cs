ffecf86c4ea10c2493d8da0356c53802007d4a62
drzbida <55928036+drzbida@users.noreply.github.com>
2025-01-06 16:11:54 +0200

    feat(commands): add configurable timeout for the commands

--- src/Mtlq/Commands/BaseCommand.cs
+++ src/Mtlq/Commands/BaseCommand.cs
@@ -2,6 +2,7 @@ using System;
 using System.CommandLine;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
+using System.Threading;
 using System.Threading.Tasks;
 using Mtlq.Models;
 
@@ -10,13 +11,21 @@ namespace Mtlq.Commands;
 public abstract class BaseCommand<T>(string name, string description, JsonTypeInfo<T> typeInfo)
     : Command(name, description)
 {
+    protected virtual int TimeoutMs => 2000;
+
     protected async Task<int> WrapExecuteAsync(Func<Task<T>> executeAsync)
     {
         try
         {
-            var result = await executeAsync();
+            var cts = new CancellationTokenSource(TimeoutMs);
+            var result = await executeAsync().WaitAsync(cts.Token);
             WriteResponse(result);
             return 0;
+        }
+        catch (OperationCanceledException)
+        {
+            WriteError(new TimeoutException($"Command timed out after {TimeoutMs} ms."));
+            return 1;
         }
         catch (Exception ex)
         {
